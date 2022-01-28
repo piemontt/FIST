42ad3bfa0600c3035c22e6bb3b95fcc34fff1fe6
Dmitry Pisanov <dima.pisanov@gmail.com>
2022-01-28 13:59:35 +0300

    28.01.2022 13:58 -upd script (arowup & down of hours), added comments for index.html

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let num1 = document.querySelector( '.number1' )
-
-num1 = parseInt(num1.textContent)
-
-console.log(num1)
 
 
-let button1 = document.getElementById( 'buttonSetTimer' )
 
-button1.onclick = function() {
-	alert('PRESSED button 1')
-	num1 +=1
+let num1In = document.querySelector( '.number1' )  /*get value of HOURS*/
 
-	console.log(num1)
-	document.querySelector( '.number1' ).textContent = num1
+num1Out = parseInt(num1In.textContent)
+
+console.log(num1Out)
+
+
+
+
+let arrowUp1 = document.querySelector( '.arrowup1' ) /* First arrow up +1 (HOURS) */
+
+arrowUp1.onclick = function() {
+	
+	num1Out +=1
+	console.log(num1Out)
+	num1In.textContent = num1Out
+
 }
 
 
+let arrowDown1 = document.querySelector( '.arrowdown1' ) /* First arrow down -1 (HOURS) */
+
+arrowDown1.onclick = function() {
+	
+	num1Out -=1
+	console.log(num1Out)
+	num1In.textContent = num1Out
+
+}
